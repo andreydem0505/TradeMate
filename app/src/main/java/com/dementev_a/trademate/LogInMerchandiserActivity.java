cc7ecca0db18eb926cf758783e54026756bc753c
Andrey Dementev <andreydem42@gmail.com>
2021-01-21 16:04:30 +0300

    auth

---
+++ app/src/main/java/com/dementev_a/trademate/LogInMerchandiserActivity.java
@@ -0,0 +1,14 @@
+package com.dementev_a.trademate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogInMerchandiserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_merchandiser);
+    }
+}
