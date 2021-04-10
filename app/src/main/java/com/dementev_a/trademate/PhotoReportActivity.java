d97b8f86955d4b2ce8d709da88548bc290cd761e
Andrey Dementev <andreydem42@gmail.com>
2021-04-10 14:00:41 +0300

    photo

--- app/src/main/java/com/dementev_a/trademate/PhotoReportActivity.java
+++ app/src/main/java/com/dementev_a/trademate/PhotoReportActivity.java
@@ -3,10 +3,17 @@ package com.dementev_a.trademate;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -16,11 +23,16 @@ import com.dementev_a.trademate.intent.IntentConstants;
 import com.dementev_a.trademate.preferences.SharedPreferencesEngine;
 import com.dementev_a.trademate.widgets.ReactOnStatus;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+
 public class PhotoReportActivity extends AppCompatActivity {
+    private ImageView photoIV;
     private TextView header, errorTV;
     private ProgressBar progressBar;
     private String name, accessToken;
     private API api;
+    private static final int REQUEST_TAKE_PHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +41,7 @@ public class PhotoReportActivity extends AppCompatActivity {
         header = findViewById(R.id.photo_report_activity_header);
         progressBar = findViewById(R.id.photo_report_activity_progress_bar);
         errorTV = findViewById(R.id.photo_report_activity_error_tv);
+        photoIV = findViewById(R.id.photo);
         name = getIntent().getStringExtra(IntentConstants.PHOTO_REPORT_NAME_INTENT_KEY);
         header.setText(name);
         SharedPreferencesEngine spe = new SharedPreferencesEngine(this, getString(R.string.shared_preferences_user));
@@ -58,4 +71,29 @@ public class PhotoReportActivity extends AppCompatActivity {
             }.execute();
         }
     };
+
+    public void onAddPhotoClickBtn(View v) {
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePhotoIntent, REQUEST_TAKE_PHOTO);
+        } catch (ActivityNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap thumbnailBitmap = (Bitmap) extras.get("data");
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            thumbnailBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+            thumbnailBitmap.recycle();
+            System.out.println(Arrays.toString(byteArray));
+            Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+            photoIV.setImageBitmap(bitmap);
+        }
+    }
 }
